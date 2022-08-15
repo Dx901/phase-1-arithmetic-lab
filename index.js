31eb8a7c2aabb6e1af140c89dc5a7c65370b43e4
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-08-15 20:25:29 +0300

    Completed assignment-arithmeticlab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code
+let num1 = 31;
+let num2 = 2;
+
+var multiply = num1*num2;
+
+let num3 = 20;
+let num4 = 16;
+var mod = 20 % 16;
+
+var max = Math.max(12, 20);
+
+var random = Math.floor(Math.random()*15) + 1;
