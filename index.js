59d8c23a437211aa1b26e7926ae4871ce80af9d7
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-08-24 20:27:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-// Write your code
 let num1 = 31;
 let num2 = 2;
 
@@ -7,7 +5,7 @@ var multiply = num1*num2;
 
 let num3 = 20;
 let num4 = 16;
-var mod = 20 % 16;
+var mod = num3 % num4;
 
 var max = Math.max(12, 20);
 
